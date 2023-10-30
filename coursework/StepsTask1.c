39b6a6ca97a7a6451f7d73a9881ed7f10f13880e
Rojan Karki <146235130+rjkr04@users.noreply.github.com>
2023-10-30 16:50:47 +0000

    Coursework finished

--- coursework/StepsTask1.c
+++ coursework/StepsTask1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "utils.h"
+
+FILE *open_file(char filename[], char mode[]);
 
 typedef struct {
 	char date[11];
@@ -39,6 +40,38 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    FITNESS_DATA data_array[200];
+    char filename[50] = "FitnessData_2023.csv";
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    int count = 0;
 
+    FILE *file = open_file(filename, "r");
 
+    char date[11];
+    char time[7];
+    char steps[7];
+
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer, ",", date, time, steps);
+        strcpy(data_array[count].date, date);
+        strcpy(data_array[count].time, time);
+        data_array[count].steps = atoi(steps);
+        count ++;
+    }
+    printf("Number of records in file: %d\n", count);
+    for (int i = 0; i < 3; i ++) {
+        printf("%s/%s/%d\n", data_array[i].date, data_array[i].time, data_array[i].steps);
+    }
+    fclose(file);
+    return 0;
+}
+
+FILE *open_file(char filename[], char mode[]) {
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) {
+        perror("");
+        exit(1);
+    }
+    return file;
 }
--- coursework/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#include <stdio.h>
-#include <stdlib.h>
-
-FILE *open_file(char filename[], char mode[]);
-
-#endif /* UTILS_H */
