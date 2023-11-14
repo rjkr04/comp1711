e6585339271497eac8b9dd2136f09b25b897a3bf
Rojan Karki <146235130+rjkr04@users.noreply.github.com>
2023-11-14 04:01:48 +0000

    coursework task 2 started, week7

--- coursework/FinessDataStruct.h
+++ coursework/FinessDataStruct.h
@@ -10,11 +10,20 @@ typedef struct {
 	int steps;
 } FITNESS_DATA;
 
+FITNESS_DATA data_array[200];
+int buffer_size = 100;
+char line_buffer[buffer_size];
+
+void printmenu()
+{
+    printf("A: Specify the filename to be imported \nB: Display the total number of records in the file \nC: Find the date and time of the timeslot with the fewest steps \nD: Find the data and time of the timeslot with the largest number of steps \nE: Find the mean step count of all the records in the file \nF: Find the longest continuous period where the step count is above 500 steps \nQ: Exit");
+}
+
 FILE *open_file(char filename[], char mode[]) {
     FILE *file = fopen(filename, mode);
     if (file == NULL) {
         perror("Error: could not open file ");
-        exit(1);
+        return(1);
     }
     return file;
 }
---
+++ coursework/StepCounter.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
+
+// Struct moved to header file
+
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+
+
+// Complete the main function
+int main() {
+    printmenu();
+    choice = getchar();
+
+        // this gets rid of the newline character which the user will enter
+        // as otherwise this will stay in the stdin and be read next time
+        while (getchar() != '\n');
+
+
+        // switch statement to control the menu.
+        switch (choice)
+        {
+        // this allows for either capital or lower case
+        case 'A':
+        case 'a':
+            counter = 0;
+            FILE *input = fopen(filename, "r");
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                counter++;
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                printf("%s - Blood iron: %.1f\n", daily_readings[i].date, daily_readings[i].bloodIron);
+            }
+            fclose(input);
+            break;
+
+        case 'B':
+        case 'b':
+            counter = 0;
+            while (fgets(line, buffer_size, input))
+            {
+                // split up the line and store it in the right place
+                // using the & operator to pass in a pointer to the bloodIron so it stores it
+                tokeniseRecord(line, ",", daily_readings[counter].date, &daily_readings[counter].bloodIron);
+                mean += daily_readings[counter].bloodIron;
+                counter++;
+            }
+            mean /= counter;
+            printf("Your average blood iron was %.2f\n", mean);
+            fclose(input);
+            break;
+
+        case 'C':
+        case 'c':
+            return 0;
+            break;
+
+        case 'D':
+        case 'd':
+            return 0;
+            break;
+
+        case 'E':
+        case 'e':
+            return 0;
+            break;
+
+        case 'F':
+        case 'f':
+            return 0;
+            break;
+
+        case 'G':
+        case 'g':
+            return 0;
+            break;
+
+        case 'Q':
+        case 'q':
+            return 0;
+            break;
+
+        // if they type anything else:
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }
+    
